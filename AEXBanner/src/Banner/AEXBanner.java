fbbfd1938acf583b6829824f1d6f70110530e8d8
jeroen <jeroen@Jeroen-Laptop>
2015-03-24 13:59:42 +0100

    AEXBanner

--- AEXBanner/src/Banner/AEXBanner.java
+++ AEXBanner/src/Banner/AEXBanner.java
@@ -5,15 +5,56 @@
  */
 package Banner;
 
+import javafx.animation.AnimationTimer;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Jeroen Hendriks
  */
-public class AEXBanner {
+public class AEXBanner extends javafx.application.Application {
 
+    AnimationTimer at = new AnimationTimer() {
+
+        @Override
+        public void handle(long now) {
+            
+        }
+    };
+    
     public void setKoersen(String koersen)
     {
         
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AEXBanner.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+   
+    public void handle()
+    {
+        
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
     
 }
