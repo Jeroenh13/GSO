f77aa05999efd7d9ec1651f37f2ad0358993a174
jeroenh13@live.nl <Sluiskamp33!>
2015-03-28 23:09:38 +0100

    Revert "banner scrollt"
    This reverts commit 1e3797b2d73d1923b3ac927d3f66b4e586601572.

--- AEXBanner/src/Banner/BannerController.java
+++ AEXBanner/src/Banner/BannerController.java
@@ -1,40 +1,14 @@
-import java.util.Timer;
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banner;
 
-
-public class BannerController extends Application {
-
-    private AEXBanner banner;
-//  private IEffectenBeurs effectenbeurs;
-
-    @Override
-    public void start(Stage primaryStage) {
-        banner = new AEXBanner();
-        //primaryStage acts as the common stage of the AEXBanner and the 
-        //BannerController:
-        banner.start(primaryStage);
-
-        //create a timer which polls every 2 seconds
-        Timer pollingTimer;
-        // todo
-
-        
-        //remove pollingTimer as soon as primaryStage is closing:
-        //primaryStage.setOnCloseRequest((WindowEvent we) -> {
-        //  pollingTimer.cancel();
-        //});
-    }
-
-    /**
-     * unfortunately, is needed pro forma to act as runnable class
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+/**
+ *
+ * @author Jeroen Hendriks
+ */
+public class BannerController {
+    
 }
