afe736489aaaf2809d524e9264570922fe012f1f
jeroenh13@live.nl <Sluiskamp33!>
2015-03-29 13:11:40 +0200

    banner werkt geheel

--- AEXBanner/src/Banner/BannerController.java
+++ AEXBanner/src/Banner/BannerController.java
@@ -2,7 +2,9 @@ package Banner;
 
 import Beurs.*;
 import java.util.Timer;
+import java.util.TimerTask;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -20,21 +22,32 @@ public class BannerController extends Application {
         //primaryStage acts as the common stage of the AEXBanner and the 
         //BannerController:
         banner.start(primaryStage);
-        StringBuilder sb = new StringBuilder();
-        for(IFonds iF : effectenbeurs.getKoersen())
-        {
-            sb.append(iF.getNaam()).append(":").append(iF.getKoers()).append("     ");
-        }
-        banner.setKoersen(sb.toString());
+        
         //create a timer which polls every 2 seconds
-        Timer pollingTimer;
+        
+        Timer pollingTimer = new Timer("KoersUpdate");
+        pollingTimer.scheduleAtFixedRate(new TimerTask(){
+
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        updateKoersen();                        
+                    }
+                    
+                });
+            }
+        }, 0, 2000);
+        
         // todo
 
         
         //remove pollingTimer as soon as primaryStage is closing:
-        //primaryStage.setOnCloseRequest((WindowEvent we) -> {
-        //  pollingTimer.cancel();
-        //});
+        primaryStage.setOnCloseRequest((WindowEvent we) -> {
+          pollingTimer.cancel();
+        });
     }
 
     /**
@@ -44,6 +57,16 @@ public class BannerController extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    void updateKoersen()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(IFonds iF : effectenbeurs.getKoersen())
+        {
+            sb.append(iF.getNaam()).append(":").append(iF.getKoers()).append("        ");
+        }
+        banner.setKoersen(sb.toString());
     }
 
 }
