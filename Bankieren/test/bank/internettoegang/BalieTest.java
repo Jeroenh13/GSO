b40a5e15cfdcee1440520b4b593540ca6e4cb05f
Jeroen <jeroenh13@live.nl>
2015-06-08 14:22:23 +0200

    BalieTest

--- Bankieren/test/bank/internettoegang/BalieTest.java
+++ Bankieren/test/bank/internettoegang/BalieTest.java
@@ -5,7 +5,13 @@
  */
 package bank.internettoegang;
 
+import bank.bankieren.Bank;
+import bank.bankieren.IBank;
+import bank.server.BalieServer;
 import fontys.observer.RemotePropertyListener;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,98 +24,69 @@ import static org.junit.Assert.*;
  * @author Jeroen Hendriks
  */
 public class BalieTest {
-    
-    public BalieTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    public IBalie balie;
+    public IBank bank;
+    public BalieServer balieserver;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        bank = new Bank("Rabobank");
+        try {
+            balie = new Balie(bank);
+        } catch (RemoteException ex) {
+            Logger.getLogger(BalieTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
      * Test of openRekening method, of class Balie.
      */
     @Test
-    public void testOpenRekening() {
-        System.out.println("openRekening");
-        String naam = "";
-        String plaats = "";
-        String wachtwoord = "";
-        Balie instance = null;
-        String expResult = "";
-        String result = instance.openRekening(naam, plaats, wachtwoord);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testBalie() {
+        System.out.println("Start of 'openRekening'");
+        String accName;
+        int accNumber;
+        IBankiersessie sessie;
+        try {
+            accName = balie.openRekening("", "Wijchen", "asdf");
+            if (accName == null) {
+                System.out.println("Test Succeeded name is empty");
+            }
+            accName = balie.openRekening("Jeroen", "", "asdf");
+            if (accName == null) {
+                System.out.println("Test Succeeded city is empty");
+            }
+            accName = balie.openRekening("Jeroen", "Wijchen", "adf");
+            if (accName == null) {
+                System.out.println("Test Succeeded password too short");
+            }
+            accName = balie.openRekening("Jeroen", "Wijchen", "aksidufpw");
+            if (accName == null) {
+                System.out.println("Test Succeeded password too long");
+            }
+            accName = balie.openRekening("Jeroen", "Wijchen", "asdf");
+            if (accName != null) {
+                System.out.println("Test Succeeded");
+            }
 
-    /**
-     * Test of logIn method, of class Balie.
-     */
-    @Test
-    public void testLogIn() throws Exception {
-        System.out.println("logIn");
-        String accountnaam = "";
-        String wachtwoord = "";
-        Balie instance = null;
-        IBankiersessie expResult = null;
-        IBankiersessie result = instance.logIn(accountnaam, wachtwoord);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+            System.out.println("End of 'openRekening' \n---------------------------");
 
-    /**
-     * Test of inform method, of class Balie.
-     */
-    @Test
-    public void testInform() {
-        System.out.println("inform");
-        int to = 0;
-        Balie instance = null;
-        instance.inform(to);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+            System.out.println("Start of login");
+            sessie = balie.logIn("Jeroen", "piepo");
+            if(sessie == null)
+            {
+                System.out.println("Test Succeeded, Account doesn't exist");
+            }
+            sessie = balie.logIn(accName, "asdf");
+            if(sessie != null)
+            {
+                System.out.println("Test succeeded");
+            }
+            System.out.println("End of login\n---------------------------");
+        } catch (RemoteException ex) {
+            Logger.getLogger(BalieTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-    /**
-     * Test of addListener method, of class Balie.
-     */
-    @Test
-    public void testAddListener() throws Exception {
-        System.out.println("addListener");
-        RemotePropertyListener listener = null;
-        String property = "";
-        Balie instance = null;
-        instance.addListener(listener, property);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of removeListener method, of class Balie.
-     */
-    @Test
-    public void testRemoveListener() throws Exception {
-        System.out.println("removeListener");
-        RemotePropertyListener listener = null;
-        String property = "";
-        Balie instance = null;
-        instance.removeListener(listener, property);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
