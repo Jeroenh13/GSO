6b6ec82a2ae07fee3463d75a2df02bb8c36c5f62
Phyxel <cypher@live.nl>
2015-06-09 10:57:29 +0200

    Bank Merge Fix

--- Bankieren/src/bank/bankieren/Bank.java
+++ Bankieren/src/bank/bankieren/Bank.java
@@ -7,120 +7,39 @@ import java.util.*;
 
 public class Bank implements IBank {
 
-<<<<<<< HEAD
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8728841131739353765L;
-	private Map<Integer,IRekeningTbvBank> accounts;
-        private Map<String, Integer> bankNumbers;
-	private Collection<IKlant> clients;
-	private int nieuwReknr;
-	private String name;
-
-	public Bank(String name) {
-		accounts = new HashMap<Integer,IRekeningTbvBank>();
-                bankNumbers = new HashMap<String, Integer>();
-		clients = new ArrayList<IKlant>();
-                
-                bankNumbers.put("RaboBank", 100000000);
-                bankNumbers.put("ING", 200000000);
-                bankNumbers.put("SNS", 300000000);
-                bankNumbers.put("ABN+AMRO", 400000000);
-                bankNumbers.put("ASN", 500000000);
-                
-                System.out.println(name);
-		nieuwReknr = bankNumbers.get(name);	
-		this.name = name;	
-	}
-
-	public int openRekening(String name, String city) {
-		if (name.equals("") || city.equals(""))
-			return -1;
-
-		IKlant klant = getKlant(name, city);
-		IRekeningTbvBank account = new Rekening(nieuwReknr, klant, Money.EURO);
-		accounts.put(nieuwReknr,account);
-		nieuwReknr++;
-		return nieuwReknr-1;
-	}
-
-	private IKlant getKlant(String name, String city) {
-		for (IKlant k : clients) {
-			if (k.getNaam().equals(name) && k.getPlaats().equals(city))
-				return k;
-		}
-		IKlant klant = new Klant(name, city);
-		clients.add(klant);
-		return klant;
-	}
-
-	public IRekening getRekening(int nr) {
-		return accounts.get(nr);
-	}
-
-        @Override
-	public synchronized boolean maakOver(int source, int destination, Money money)
-			throws NumberDoesntExistException {
-		if (source == destination)
-			throw new RuntimeException(
-					"cannot transfer money to your own account");
-		if (!money.isPositive())
-			throw new RuntimeException("money must be positive");
-
-		IRekeningTbvBank source_account = (IRekeningTbvBank) getRekening(source);
-		if (source_account == null)
-			throw new NumberDoesntExistException("account " + source
-					+ " unknown at " + name);
-
-		Money negative = Money.difference(new Money(0, money.getCurrency()),
-				money);
-		boolean success = source_account.muteer(negative);
-		if (!success)
-			return false;
-
-		IRekeningTbvBank dest_account = (IRekeningTbvBank) getRekening(destination);
-		if (dest_account == null) 
-			throw new NumberDoesntExistException("account " + destination
-					+ " unknown at " + name);
-		success = dest_account.muteer(money);
-
-		if (!success) // rollback
-			source_account.muteer(money);
-		return success;
-	}
-
-	@Override
-	public String getName() {
-		return name;
-	}
-=======
-    /**
-     *
-     */
     private static final long serialVersionUID = -8728841131739353765L;
     private Map<Integer, IRekeningTbvBank> accounts;
+    private Map<String, Integer> bankNumbers;
     private Collection<IKlant> clients;
     private int nieuwReknr;
     private String name;
 
     public Bank(String name) {
         accounts = new HashMap<Integer, IRekeningTbvBank>();
+        bankNumbers = new HashMap<String, Integer>();
         clients = new ArrayList<IKlant>();
-        nieuwReknr = 100000000;
+
+        bankNumbers.put("RaboBank", 100000000);
+        bankNumbers.put("ING", 200000000);
+        bankNumbers.put("SNS", 300000000);
+        bankNumbers.put("ABN+AMRO", 400000000);
+        bankNumbers.put("ASN", 500000000);
+
+        System.out.println(name);
+        nieuwReknr = bankNumbers.get(name);
         this.name = name;
     }
 
     public int openRekening(String name, String city) {
         if (name.equals("") || city.equals("")) {
             return -1;
-        } else {
-            IKlant klant = getKlant(name, city);
-            IRekeningTbvBank account = new Rekening(nieuwReknr, klant, Money.EURO);
-            accounts.put(nieuwReknr, account);
-            nieuwReknr++;
-            return nieuwReknr - 1;
         }
+
+        IKlant klant = getKlant(name, city);
+        IRekeningTbvBank account = new Rekening(nieuwReknr, klant, Money.EURO);
+        accounts.put(nieuwReknr, account);
+        nieuwReknr++;
+        return nieuwReknr - 1;
     }
 
     private IKlant getKlant(String name, String city) {
@@ -167,7 +86,6 @@ public class Bank implements IBank {
             throw new NumberDoesntExistException("account " + destination
                     + " unknown at " + name);
         }
-        
         success = dest_account.muteer(money);
 
         if (!success) // rollback
@@ -181,6 +99,5 @@ public class Bank implements IBank {
     public String getName() {
         return name;
     }
->>>>>>> origin/master
 
 }
