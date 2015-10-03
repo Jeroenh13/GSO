d2e69e0bb4054287a3d3c5a2e826b49231168f74
Jeroenh13 <jeroenh13@live.nl>
2015-10-03 16:12:30 +0200

    opent scherm

--- FireBee/src/gui/MainMenuController.java
+++ FireBee/src/gui/MainMenuController.java
@@ -5,34 +5,49 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author jeroe
+ * @author jeroen
  */
-public class MainMenuController extends Application{
-    
+public class MainMenuController extends Application {
+
+    @FXML
+    ImageView imgPlattegrond;
+
     @Override
     public void start(Stage stage) throws Exception {
-       
+
         Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
-        
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    
-        /**
+
+    @FXML
+    private void gotoPlattegrond(MouseEvent event) {
+        gotoNextScreen("Plattegrond");
+    }
+
+    /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
      * launched through deployment artifacts, e.g., in IDEs with limited FX
@@ -42,5 +57,21 @@ public class MainMenuController extends Application{
      */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void gotoNextScreen(String input){
+        if ("Plattegrond".equals(input)) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("Plattegrond-2.fxml"));
+                
+                Stage stage = new Stage();
+                
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
